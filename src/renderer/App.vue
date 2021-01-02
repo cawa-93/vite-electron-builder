d3a614a0d6edc9c395164a16582eb2463958404a
Alex Kozack <kozackunisoft@gmail.com>
2021-01-02 16:19:30 +0200

    ci: typecheck

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
-  {{ data }}
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <p>{{ a }}</p>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-import {useElectron} from '/@/use/electron';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
-  },
   setup() {
-    const {data} = useElectron();
-
-    return {data};
+    const a: string = 111;
+    return {a};
   },
 });
 </script>
