97d18d681d950c155fe15b4bee30a6a0ec374e75
Alex Kozack <cawa-93@users.noreply.github.com>
2021-01-04 11:29:21 +0200

    feat: Add vue-router (#6)

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -3,25 +3,29 @@
     alt="Vue logo"
     src="./assets/logo.png"
   >
-  {{ data }}
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <app-navigation />
+  <router-view />
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
-import HelloWorld from '/@/components/HelloWorld.vue';
-import {useElectron} from '/@/use/electron';
+import AppNavigation from '/@/components/AppNavigation.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
-  },
-  setup() {
-    const {data} = useElectron();
-
-    return {data};
+    AppNavigation,
   },
 });
 </script>
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
