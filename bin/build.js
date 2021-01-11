0740f395e3f204a35b859d4a35ca70508a333a2d
Alex Kozack <kozackunisoft@gmail.com>
2021-01-11 11:49:15 +0200

    build: Add Bundling time

--- bin/build.js
+++ bin/build.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+console.time('Bundling time');
 const {build} = require('vite');
 const {join} = require('path');
 
@@ -12,35 +13,36 @@ const entryPoints = [
 
 const buildEntryPoint = (configFile) => build({configFile, mode});
 
-const generatePacjJson = () => {
+const generatePackageJson = () => {
   const {writeFile} = require('fs/promises');
-  const pjson = require(join(process.cwd(), 'package.json'));
+  const packageJson = require(join(process.cwd(), 'package.json'));
 
   // Remove all bundled dependencies
   const external = require('../config/external-packages');
   for (const type of ['dependencies', 'devDependencies', 'optionalDependencies']) {
-    if (pjson[type] === undefined) {
+    if (packageJson[type] === undefined) {
       continue;
     }
 
-    for (const key of Object.keys(pjson[type])) {
+    for (const key of Object.keys(packageJson[type])) {
       if (!external.includes(key)) {
-        delete pjson[type][key];
+        delete packageJson[type][key];
       }
     }
   }
 
   // Set version
   const now = new Date;
-  pjson.version = `${now.getFullYear() - 2000}.${now.getMonth() + 1}.${now.getDate()}`;
+  packageJson.version = `${now.getFullYear() - 2000}.${now.getMonth() + 1}.${now.getDate()}`;
 
   // Create new package.json
-  return writeFile(join(process.cwd(), 'dist/source/package.json'), JSON.stringify(pjson));
+  return writeFile(join(process.cwd(), 'dist/source/package.json'), JSON.stringify(packageJson));
 };
 
 Promise.all(entryPoints.map(buildEntryPoint))
-  .then(generatePacjJson)
+  .then(generatePackageJson)
   .catch(e => {
     console.error(e);
     process.exit(1);
-  });
+  })
+  .finally(() => console.timeEnd('Bundling time'));
