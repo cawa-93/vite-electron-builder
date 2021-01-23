2b88db0d94c3f598bde4d893831548315e001ffd
Alex Kozack <kozackunisoft@gmail.com>
2021-01-23 15:37:35 +0200

    refactor: Add error message for electron-releases

--- config/electron-dep-versions.js
+++ config/electron-dep-versions.js
@@ -4,6 +4,15 @@ const releases = require('electron-releases/lite.json');
 
 const release = releases.find(r => r.version === version);
 
+if (!release) {
+  throw new Error(`Can't find electron release info for version: ${version}
+  Try run:
+
+  npm update electron-releases
+
+  And try again`);
+}
+
 module.exports.chrome = release.deps.chrome.split('.')[0];
 module.exports.node = release.deps.node;
 
