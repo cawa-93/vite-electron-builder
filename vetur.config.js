829a6b317e21ea54db9753cc109b6a9bb3a550b5
xiaohulu <idocument@qq.com>
2021-02-24 16:43:13 +0800

    fix(vetur): added vetur config for path alias

---
+++ vetur.config.js
@@ -0,0 +1,21 @@
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  settings: {
+    "vetur.useWorkspaceDependencies": true,
+    "vetur.experimental.templateInterpolationService": true
+  },
+  projects: [
+    {
+      root: './src/renderer',
+      tsconfig: './tsconfig.json',
+      snippetFolder: './.vscode/vetur/snippets',
+      globalComponents: [
+        './components/**/*.vue'
+      ]
+    },
+    {
+      root: './src/main',
+      tsconfig: './tsconfig.json'
+    }
+  ]
+}
