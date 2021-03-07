89303ce528434ae00546256f9e225a4a1ee33959
Alex Kozack <cawa-93@users.noreply.github.com>
2021-03-08 01:00:17 +0200

    refactor: bin/watch.js (#147)

--- bin/watch.js
+++ bin/watch.js
@@ -1,101 +1,141 @@
 #!/usr/bin/node
 
-// WARNING! Almost everything in this file needs a better approach.
-
 // TODO:
-// - Do not use an watcher from Vite. Use your own watcher, or wait for a solution - https://github.com/vitejs/vite/issues/1434
-// - Find the best way to start / restart an electron. Logging from child process.
 // - Disable dependency optimization during development.
-// - Examine the behavior of vite when starting `build` command but with mode = `development`
-// - Refactoring and fix duplicate code
-
+// - Refactoring
 
 const slash = require('slash');
+const chokidar = require('chokidar');
 const {createServer, build, normalizePath} = require('vite');
+const electronPath = require('electron');
+const {spawn} = require('child_process');
 const {join, relative} = require('path');
 
 const mode = process.env.MODE || 'development';
 
-function startElectron() {
-  const {spawn} = require('child_process');
+const TIMEOUT = 500;
 
-  const electronPath = require('electron');
-
-  return spawn(electronPath, [join(process.cwd(), 'dist/source/main/index.cjs.js')]);
+function debounce(f, ms) {
+  let isCoolDown = false;
+  return function () {
+    if (isCoolDown) return;
+    f.apply(this, arguments);
+    isCoolDown = true;
+    setTimeout(() => isCoolDown = false, ms);
+  };
 }
 
-
 (async () => {
-  // Create Vite dev server
-  const server = await createServer({
+
+// Create Vite dev server
+  const viteDevServer = await createServer({
     mode,
     configFile: join(process.cwd(), 'config/renderer.vite.js'),
   });
 
-
-  // Build preload entrypoint
-  const buildPreload = () => build({mode, configFile: join(process.cwd(), 'config/preload.vite.js')});
-  await buildPreload();
-
-  // Watch `src/preload` and rebuild preload entrypoint
-  server.watcher.add(join(process.cwd(), 'src/preload/**'));
-  server.watcher.on('change', (file) => {
-    file = normalizePath(file);
-
-    if (!file.includes('/src/preload/')) {
-      return;
-    }
-
-    return buildPreload();
-  });
-
-  // Reload page on preload script change
-  server.watcher.add(join(process.cwd(), 'dist/source/preload/**'));
-  server.watcher.on('change', (file) => {
-    file = normalizePath(file);
-
-    if (!file.includes('/dist/source/preload/')) {
-      return;
-    }
-
-    server.ws.send({
-      type: 'full-reload',
-      path: '/' + slash(relative(server.config.root, file)),
-    });
-  });
-
-  // Run Vite server
-  await server.listen();
+  await viteDevServer.listen();
 
 
-  // Determining the current URL of the server. It depend on /config/renderer.vite.js
-  // Write a value to an environment variable to pass it to the main process.
+// Determining the current URL of the server. It depend on /config/renderer.vite.js
+// Write a value to an environment variable to pass it to the main process.
   {
-    const protocol = `http${server.config.server.https ? 's' : ''}:`;
-    const host = server.config.server.host || 'localhost';
-    const port = server.config.server.port; // Vite searches for and occupies the first free port: 3000, 3001, 3002 and so on
+    const protocol = `http${viteDevServer.config.server.https ? 's' : ''}:`;
+    const host = viteDevServer.config.server.host || 'localhost';
+    const port = viteDevServer.config.server.port; // Vite searches for and occupies the first free port: 3000, 3001, 3002 and so on
     const path = '/';
     process.env.VITE_DEV_SERVER_URL = `${protocol}//${host}:${port}${path}`;
   }
 
-  // Build main entrypoint
-  const buildMain = () => build({mode, configFile: join(process.cwd(), 'config/main.vite.js')});
-  await buildMain();
 
-  // Watch `src/main` and rebuild main entrypoint
-  server.watcher.add(join(process.cwd(), 'src/main/**'));
-  server.watcher.on('change', (file) => {
-    file = normalizePath(file);
-
-    if (!file.includes('/src/main/')) {
-      return;
+  /** @type {ChildProcessWithoutNullStreams | null} */
+  let spawnProcess = null;
+  const runMain = debounce(() => {
+    if (spawnProcess !== null) {
+      spawnProcess.kill('SIGINT');
+      spawnProcess = null;
     }
 
-    return buildMain();
-  });
+    spawnProcess = spawn(electronPath, [join(process.cwd(), 'dist/source/main/index.cjs.js')]);
+
+    spawnProcess.stdout.on('data', d => console.log(d.toString()));
+    spawnProcess.stderr.on('data', d => console.error(d.toString()));
+
+    return spawnProcess;
+
+  }, TIMEOUT);
+
+  const buildMain = () => {
+    return build({mode, configFile: join(process.cwd(), 'config/main.vite.js')});
+  };
+
+  const buildMainDebounced = debounce(buildMain, TIMEOUT);
+
+  const runPreload = debounce((file) => {
+    viteDevServer.ws.send({
+      type: 'full-reload',
+      path: '/' + slash(relative(viteDevServer.config.root, file)),
+    });
+
+  }, TIMEOUT);
+
+  const buildPreload = () => {
+    return build({mode, configFile: join(process.cwd(), 'config/preload.vite.js')});
+  };
+
+  const buildPreloadDebounced = debounce(buildPreload, TIMEOUT);
 
 
-  // Run electron app
-  startElectron();
+  await Promise.all([
+    buildMain(),
+    buildPreload(),
+  ]);
+
+
+  const watcher = chokidar.watch([
+    join(process.cwd(), 'src/main/**'),
+    join(process.cwd(), 'src/preload/**'),
+    join(process.cwd(), 'dist/source/main/**'),
+    join(process.cwd(), 'dist/source/preload/**'),
+  ], {ignoreInitial: true});
+
+
+  watcher
+    .on('unlink', path => {
+      const normalizedPath = normalizePath(path);
+      if (spawnProcess !== null && normalizedPath.includes('/dist/source/main/')) {
+        spawnProcess.kill('SIGINT');
+        spawnProcess = null;
+      }
+    })
+    .on('add', path => {
+      const normalizedPath = normalizePath(path);
+      if (normalizedPath.includes('/dist/source/main/')) {
+        return runMain();
+      }
+
+      if (spawnProcess !== undefined && normalizedPath.includes('/dist/source/preload/')) {
+        return runPreload(normalizedPath);
+      }
+    })
+    .on('change', (path) => {
+      const normalizedPath = normalizePath(path);
+
+      if (normalizedPath.includes('/src/main/')) {
+        return buildMainDebounced();
+      }
+
+      if (normalizedPath.includes('/dist/source/main/')) {
+        return runMain();
+      }
+
+      if (normalizedPath.includes('/src/preload/')) {
+        return buildPreloadDebounced();
+      }
+
+      if (normalizedPath.includes('/dist/source/preload/')) {
+        return runPreload(normalizedPath);
+      }
+    });
+
+  await runMain();
 })();
-
