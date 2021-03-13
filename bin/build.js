fc196f879e116254278466a98ec5b58f5aaafaf8
Alex Kozack <kozackunisoft@gmail.com>
2021-03-13 22:33:44 +0200

    refactor: build script

--- bin/build.js
+++ bin/build.js
@@ -1,29 +1,41 @@
 #!/usr/bin/node
-console.time('Bundling time');
 const {build} = require('vite');
 const {join} = require('path');
 
 /** @type 'production' | 'development' | 'test' */
 const mode = process.env.MODE || 'production';
 
-const configs = [
-  join(process.cwd(), 'packages/main/vite.config.ts'),
-  join(process.cwd(), 'packages/preload/vite.config.ts'),
-  join(process.cwd(), 'packages/renderer/vite.config.ts'),
-];
+const packagesMap = new Map([
+  ['main', join(process.cwd(), 'packages/main/vite.config.ts')],
+  ['preload', join(process.cwd(), 'packages/preload/vite.config.ts')],
+  ['renderer', join(process.cwd(), 'packages/renderer/vite.config.ts')],
+]);
 
 
 /**
  * Run `vite build` for config file
- * @param {string} configFile
- * @return {Promise<RollupOutput | RollupOutput[]>}
  */
 const buildByConfig = (configFile) => build({configFile, mode});
+(async () => {
+  try {
+    const totalTimeLabel = 'Total bundling time';
+    console.time(totalTimeLabel);
 
+    for (const [packageName, packageConfigPath] of packagesMap) {
+      console.group(`Build ${packageName}:`);
 
-Promise.all(configs.map(buildByConfig))
-  .then(() => console.timeEnd('Bundling time'))
-  .catch(e => {
+      const timeLabel = `Build ${packageName} time`;
+      console.time(timeLabel);
+
+      await buildByConfig(packageConfigPath);
+
+      console.timeEnd(timeLabel);
+      console.groupEnd();
+      console.log('\n'); // Just for pretty print
+    }
+    console.timeEnd(totalTimeLabel);
+  } catch (e) {
     console.error(e);
     process.exit(1);
-  });
+  }
+})();
