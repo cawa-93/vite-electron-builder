67d7f3a01651d1ffcde94dc533f02bb8dc9d4234
Alex Kozack <kozackunisoft@gmail.com>
2021-03-14 10:36:54 +0200

    shims-vue.d.ts

--- packages/renderer/types/shims-vue.d.ts
+++ packages/renderer/types/shims-vue.d.ts
@@ -1,5 +1,6 @@
 declare module '*.vue' {
-  import type {DefineComponent} from 'vue';
-  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, never>;
+  import { DefineComponent } from 'vue';
+  // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
+  const component: DefineComponent<{}, {}, any>;
   export default component;
 }
