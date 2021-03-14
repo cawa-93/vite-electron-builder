15b6aba1edd4da09d776ad9868f3a2ca829de535
Alex Kozack <kozackunisoft@gmail.com>
2021-03-14 11:39:33 +0200

    try

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,15 +1,7 @@
 import {createApp} from 'vue';
 import App from '/@/App.vue';
 import router from '/@/router';
-import {useElectron} from '/@/use/electron';
 
 createApp(App)
   .use(router)
   .mount('#app');
-
-
-  const a = useElectron();
-  a.versions.push();
-
-  const e :ElectronApi = {versions: {}};
-  e.versions;
