a9bd8a5dc00a7ebfcb29933246966dc69dbde0ba
Alex Kozack <kozackunisoft@gmail.com>
2021-03-15 10:47:37 +0200

    Fix: Default build mode

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,7 @@ const {build, loadEnv} = require('vite');
 const {dirname} = require('path');
 
 /** @type 'production' | 'development' | 'test' */
-const mode = process.env.MODE = process.env.MODE || 'development';
+const mode = process.env.MODE = process.env.MODE || 'production';
 
 /**
  * Vite looks for `.env` files only in "packages/**" directories.
