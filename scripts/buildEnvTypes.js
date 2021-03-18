738f7e35e6a5f412edee660598fb805c819c2069
Alex Kozack <kozackunisoft@gmail.com>
2021-03-18 14:46:41 +0200

    refactor: scripts/buildEnvTypes.js

--- scripts/buildEnvTypes.js
+++ scripts/buildEnvTypes.js
@@ -4,32 +4,65 @@ const {resolveConfig} = require('vite')
 const {writeFileSync, mkdirSync, existsSync} = require('fs')
 const {resolve, dirname} = require('path')
 
+const MODES = ['production', 'development', 'test']
+
+const configFile = resolve(process.cwd(), './packages/main/vite.config.js')
+
+const typeDefinitionFile = resolve(process.cwd(), './types/env.d.ts')
+
+/**
+ *
+ * @return {string}
+ */
+function getBaseInterface() {
+  return 'interface IBaseEnv {[key: string]: string}'
+}
+
+/**
+ *
+ * @param {string} mode
+ * @return {Promise<{name: string, declaration: string}>}
+ */
+async function getInterfaceByMode(mode) {
+  const interfaceName = `${mode}Env`
+  const {env: envForMode} = await resolveConfig({mode, configFile}, 'build')
+  return {
+    name: interfaceName,
+    declaration: `interface ${interfaceName} extends IBaseEnv ${JSON.stringify(envForMode)}`,
+  }
+}
+
 /**
  * @param {string[]} modes
  * @param {string} filePath
  */
 async function buildMode(modes, filePath) {
-  const interfaces = await Promise.all(modes.map(async mode => {
-    const modeInterfaceName = `${mode}Env`
-    const {env} = await resolveConfig({mode, configFile: resolve(process.cwd(), 'packages/main/vite.config.js')}, 'build')
 
-    const interfaceDeclaration = `interface ${modeInterfaceName} ${JSON.stringify(env)}`
+  const IBaseEnvDeclaration = getBaseInterface()
 
-    return {modeInterfaceName, interfaceDeclaration}
-  }))
+  const interfaces = await Promise.all(modes.map(getInterfaceByMode))
 
-  const interfacesDeclarations = interfaces.map(({interfaceDeclaration}) => interfaceDeclaration).join('\n')
-  const type = interfaces.map(({modeInterfaceName}) => modeInterfaceName).join(' | ')
+  const allDeclarations = interfaces.map(i => i.declaration)
+  const allNames = interfaces.map(i => i.name)
+
+  const ImportMetaEnvDeclaration = `type ImportMetaEnv = Readonly<${allNames.join(' | ')}>`
+
+  const content = `
+    ${IBaseEnvDeclaration}
+    ${allDeclarations.join('\n')}
+    ${ImportMetaEnvDeclaration}
+  `
 
   const dir = dirname(filePath)
   if (!existsSync(dir)) {
     mkdirSync(dir)
   }
 
-  writeFileSync(filePath, `${interfacesDeclarations}\ntype ImportMetaEnv = ${type}\n`, {encoding: 'utf-8', flag: 'w'})
+
+  writeFileSync(filePath, content, {encoding: 'utf-8', flag: 'w'})
 }
 
-buildMode(['production', 'development', 'test'], resolve(process.cwd(), './types/env.d.ts'))
+buildMode(MODES, typeDefinitionFile)
   .catch(err => {
     console.error(err)
     process.exit(1)
