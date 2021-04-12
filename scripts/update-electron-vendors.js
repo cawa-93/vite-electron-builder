9cbc5cab5a42480fa984e8b2415c52c2294c8487
Alex Kozack <kozackunisoft@gmail.com>
2021-04-12 16:41:28 +0300

    fix: update-electron-vendors.js

--- scripts/update-electron-vendors.js
+++ scripts/update-electron-vendors.js
@@ -1,6 +1,8 @@
 const {writeFile, readFile} = require('fs/promises')
 const {execSync} = require('child_process')
 const electron = require('electron')
+const path = require('path')
+
 /**
  * Returns versions of electron vendors
  * The performance of this feature is very poor and can be improved
@@ -18,7 +20,7 @@ function getVendors() {
 }
 
 
-function formatedJSON(obj) {
+function formattedJSON(obj) {
   return JSON.stringify(obj, null, 2) + '\n'
 }
 
@@ -28,22 +30,24 @@ function updateVendors() {
   const nodeMajorVersion = electronRelease.node.split('.')[0]
   const chromeMajorVersion = electronRelease.v8.split('.')[0] + electronRelease.v8.split('.')[1]
 
+  const packageJSONPath = path.resolve(process.cwd(), 'package.json')
+
   return Promise.all([
     writeFile('./electron-vendors.config.json',
-      formatedJSON({
+      formattedJSON({
         chrome: chromeMajorVersion,
         node: nodeMajorVersion,
       }),
     ),
 
-    readFile('../package.json').then(JSON.parse).then((packageJson) => {
-      if (!packageJson || !Array.isArray(packageJson.browserslist)) {
-        throw new Error('Can\'t find browserslist in package.json')
+    readFile(packageJSONPath).then(JSON.parse).then((packageJSON) => {
+      if (!packageJSON || !Array.isArray(packageJSON.browserslist)) {
+        throw new Error(`Can't find browserslist in ${packageJSONPath}`)
       }
 
-      packageJson.browserslist = [`Chrome ${chromeMajorVersion}`]
+      packageJSON.browserslist = [`Chrome ${chromeMajorVersion}`]
 
-      return writeFile('../package.json', formatedJSON(packageJson))
+      return writeFile(packageJSONPath, formattedJSON(packageJSON))
     }),
   ])
 }
