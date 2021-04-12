d4d298ab820d612b1b0c6841e11976c1ef727802
Alex Kozack <cawa-93@users.noreply.github.com>
2021-04-12 16:44:54 +0300

    chore: Reduce the number of files (#226)
    * chore: Move `lint-staged` config
    * chore: Move `browserslist` config
    * chore: Remove empty `.env` files

--- scripts/update-electron-vendors.js
+++ scripts/update-electron-vendors.js
@@ -1,6 +1,8 @@
-const {writeFile} = require('fs/promises')
+const {writeFile, readFile} = require('fs/promises')
 const {execSync} = require('child_process')
 const electron = require('electron')
+const path = require('path')
+
 /**
  * Returns versions of electron vendors
  * The performance of this feature is very poor and can be improved
@@ -18,21 +20,35 @@ function getVendors() {
 }
 
 
+function formattedJSON(obj) {
+  return JSON.stringify(obj, null, 2) + '\n'
+}
+
 function updateVendors() {
   const electronRelease = getVendors()
 
   const nodeMajorVersion = electronRelease.node.split('.')[0]
   const chromeMajorVersion = electronRelease.v8.split('.')[0] + electronRelease.v8.split('.')[1]
 
+  const packageJSONPath = path.resolve(process.cwd(), 'package.json')
+
   return Promise.all([
     writeFile('./electron-vendors.config.json',
-      JSON.stringify({
+      formattedJSON({
         chrome: chromeMajorVersion,
         node: nodeMajorVersion,
-      }, null, 2) + '\n',
+      }),
     ),
 
-    writeFile('./.browserslistrc', `Chrome ${chromeMajorVersion}\n`),
+    readFile(packageJSONPath).then(JSON.parse).then((packageJSON) => {
+      if (!packageJSON || !Array.isArray(packageJSON.browserslist)) {
+        throw new Error(`Can't find browserslist in ${packageJSONPath}`)
+      }
+
+      packageJSON.browserslist = [`Chrome ${chromeMajorVersion}`]
+
+      return writeFile(packageJSONPath, formattedJSON(packageJSON))
+    }),
   ])
 }
 
