7bf626b4b67c39187ca4d9fea1f1e97c904c8288
Alex Kozack <kozackunisoft@gmail.com>
2021-04-20 15:57:31 +0300

    refactor(main): `src/index.ts`

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,10 +1,14 @@
-/*
- *  TODO: Refactor this file
- */
-
 import {app, BrowserWindow} from 'electron';
 import {join} from 'path';
 import {URL} from 'url';
+
+
+const isSingleInstance = app.requestSingleInstanceLock();
+
+if (!isSingleInstance) {
+  app.quit();
+  process.exit(0);
+}
 
 /**
  * Workaround for TypeScript bug
@@ -12,75 +16,74 @@ import {URL} from 'url';
  */
 const env = import.meta.env;
 
-const gotTheLock = app.requestSingleInstanceLock();
 
-if (!gotTheLock) {
-  app.quit();
-} else {
-
-  // Install "Vue.js devtools BETA"
-  if (env.MODE === 'development') {
-    app.whenReady()
-      .then(() => import('electron-devtools-installer'))
-      .then(({default: installExtension, VUEJS3_DEVTOOLS}) => installExtension(VUEJS3_DEVTOOLS))
-      .catch(e => console.error('Failed install extension:', e));
-  }
-
-  let mainWindow: BrowserWindow | null = null;
-
-  const createWindow = async () => {
-    mainWindow = new BrowserWindow({
-      show: true,
-      webPreferences: {
-        preload: join(__dirname, '../../preload/dist/index.cjs'),
-        contextIsolation: env.MODE !== 'test',   // Spectron tests can't work with contextIsolation: true
-        enableRemoteModule: env.MODE === 'test', // Spectron tests can't work with enableRemoteModule: false
-      },
-    });
-
-    /**
-     * URL for main window.
-     * Vite dev server for development.
-     * `file://../renderer/index.html` for production and test
-     */
-    const pageUrl = env.MODE === 'development'
-      ? env.VITE_DEV_SERVER_URL
-      : new URL('../renderer/dist/index.html', 'file://' + __dirname).toString();
-
-    if (env.MODE === 'development') {
-      mainWindow.webContents.openDevTools();
-    }
-
-    await mainWindow.loadURL(pageUrl);
-  };
-
-
-  app.on('second-instance', () => {
-    // Someone tried to run a second instance, we should focus our window.
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore();
-      mainWindow.focus();
-    }
-  });
-
-
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  });
-
-
+// Install "Vue.js devtools"
+if (env.MODE === 'development') {
   app.whenReady()
-    .then(createWindow)
-    .catch((e) => console.error('Failed create window:', e));
-
-
-  // Auto-updates
-  if (env.PROD) {
-    app.whenReady()
-      .then(() => import('electron-updater'))
-      .then(({autoUpdater}) => autoUpdater.checkForUpdatesAndNotify())
-      .catch((e) => console.error('Failed check updates:', e));
-  }
+    .then(() => import('electron-devtools-installer'))
+    .then(({default: installExtension, VUEJS3_DEVTOOLS}) => installExtension(VUEJS3_DEVTOOLS, {
+      loadExtensionOptions: {
+        allowFileAccess: true,
+      },
+    }))
+    .catch(e => console.error('Failed install extension:', e));
 }
+
+let mainWindow: BrowserWindow | null = null;
+
+const createWindow = async () => {
+  mainWindow = new BrowserWindow({
+    show: true,
+    webPreferences: {
+      preload: join(__dirname, '../../preload/dist/index.cjs'),
+      contextIsolation: env.MODE !== 'test',   // Spectron tests can't work with contextIsolation: true
+      enableRemoteModule: env.MODE === 'test', // Spectron tests can't work with enableRemoteModule: false
+    },
+  });
+
+  /**
+   * URL for main window.
+   * Vite dev server for development.
+   * `file://../renderer/index.html` for production and test
+   */
+  const pageUrl = env.MODE === 'development'
+    ? env.VITE_DEV_SERVER_URL
+    : new URL('../renderer/dist/index.html', 'file://' + __dirname).toString();
+
+  if (env.MODE === 'development') {
+    mainWindow.webContents.once('dom-ready', () => mainWindow?.webContents.openDevTools());
+  }
+
+  await mainWindow.loadURL(pageUrl);
+};
+
+
+app.on('second-instance', () => {
+  // Someone tried to run a second instance, we should focus our window.
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore();
+    mainWindow.focus();
+  }
+});
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+
+app.whenReady()
+  .then(createWindow)
+  .catch((e) => console.error('Failed create window:', e));
+
+
+// Auto-updates
+if (env.PROD) {
+  app.whenReady()
+    .then(() => import('electron-updater'))
+    .then(({autoUpdater}) => autoUpdater.checkForUpdatesAndNotify())
+    .catch((e) => console.error('Failed check updates:', e));
+}
+
