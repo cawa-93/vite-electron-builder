b5a4cf8ec9ff492d39373b422a93739e9d7f3520
Alex Kozack <cawa-93@users.noreply.github.com>
2021-07-23 10:08:57 +0300

    fix: `production` as default build mode

--- scripts/loadAndSetEnv.mjs
+++ scripts/loadAndSetEnv.mjs
@@ -11,7 +11,7 @@ const {loadEnv} = require('vite')
  * @return {void}
  */
 export function loadAndSetEnv(mode, cwd) {
-  const env = loadEnv(mode, cwd)
+  const env = loadEnv(mode || 'production', cwd)
   for (const envKey in env) {
     if (process.env[envKey] === undefined && env.hasOwnProperty(envKey)) {
       process.env[envKey] = env[envKey]
