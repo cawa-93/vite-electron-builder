2297de5be4fb46dec989d4965267b86ae78078cc
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2021-10-15 17:22:51 +0200

    adds missing semi

--- scripts/watch.js
+++ scripts/watch.js
@@ -82,7 +82,7 @@ const setupMainPackageWatcher = (viteDevServer) => {
       spawnProcess.stderr.on('data', d => {
         const data = d.toString().trim();
         if (!data) return;
-        const mayIgnore = stderrFilterPatterns.some((r) => r.test(data))
+        const mayIgnore = stderrFilterPatterns.some((r) => r.test(data));
         if (mayIgnore) return;
         logger.error(data, { timestamp: true });
       });
