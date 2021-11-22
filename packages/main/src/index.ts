830f4f0fa87008142c0b54c4871ec5c7cb374206
AN <dharmaturtle@users.noreply.github.com>
2021-11-22 03:16:52 -0600

    feat: Better default security for `web-contents-created` (#558)
    * Safer `setWindowOpenHandler`
    * added isDevelopment
    * Disable navigation
    * use Set<`https://${string}`> to enforce protocol
    * permit live reload of index.html
    * Improved comments & names

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -4,6 +4,7 @@ import {URL} from 'url';
 
 
 const isSingleInstance = app.requestSingleInstanceLock();
+const isDevelopment = import.meta.env.MODE === 'development';
 
 if (!isSingleInstance) {
   app.quit();
@@ -13,7 +14,7 @@ if (!isSingleInstance) {
 app.disableHardwareAcceleration();
 
 // Install "Vue.js devtools"
-if (import.meta.env.MODE === 'development') {
+if (isDevelopment) {
   app.whenReady()
     .then(() => import('electron-devtools-installer'))
     .then(({default: installExtension, VUEJS3_DEVTOOLS}) => installExtension(VUEJS3_DEVTOOLS, {
@@ -44,19 +45,9 @@ const createWindow = async () => {
   mainWindow.on('ready-to-show', () => {
     mainWindow?.show();
 
-    if (import.meta.env.MODE === 'development') {
+    if (isDevelopment) {
       mainWindow?.webContents.openDevTools();
     }
-  });
-
-  /**
-   * External hyperlinks open in the default browser.
-   *
-   * @see https://stackoverflow.com/a/67409223
-   */
-   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
-    shell.openExternal(url);
-    return { action: 'deny' };
   });
 
   /**
@@ -64,13 +55,60 @@ const createWindow = async () => {
    * Vite dev server for development.
    * `file://../renderer/index.html` for production and test
    */
-  const pageUrl = import.meta.env.MODE === 'development' && import.meta.env.VITE_DEV_SERVER_URL !== undefined
+  const pageUrl = isDevelopment && import.meta.env.VITE_DEV_SERVER_URL !== undefined
     ? import.meta.env.VITE_DEV_SERVER_URL
     : new URL('../renderer/dist/index.html', 'file://' + __dirname).toString();
 
 
   await mainWindow.loadURL(pageUrl);
 };
+
+ app.on('web-contents-created', (_event, contents) => {
+
+  /**
+   * Block navigation to origins not on the allowlist.
+   *
+   * Navigation is a common attack vector. If an attacker can convince the app to navigate away
+   * from its current page, they can possibly force the app to open web sites on the Internet.
+   *
+   * @see https://www.electronjs.org/docs/latest/tutorial/security#13-disable-or-limit-navigation
+   */
+  contents.on('will-navigate', (event, url) => {
+    const allowedOrigins : ReadonlySet<string> =
+      new Set<`https://${string}`>(); // Do not use insecure protocols like HTTP. https://www.electronjs.org/docs/latest/tutorial/security#1-only-load-secure-content
+    const { origin, hostname } = new URL(url);
+    const isDevLocalhost = isDevelopment && hostname === 'localhost'; // permit live reload of index.html
+    if (!allowedOrigins.has(origin) && !isDevLocalhost){
+      console.warn('Blocked navigating to an unallowed origin:', origin);
+      event.preventDefault();
+    }
+  });
+
+  /**
+  * Hyperlinks to allowed sites open in the default browser.
+  *
+  * The creation of new `webContents` is a common attack vector. Attackers attempt to convince the app to create new windows,
+  * frames, or other renderer processes with more privileges than they had before; or with pages opened that they couldn't open before.
+  * You should deny any unexpected window creation.
+  *
+  * @see https://www.electronjs.org/docs/latest/tutorial/security#14-disable-or-limit-creation-of-new-windows
+  * @see https://www.electronjs.org/docs/latest/tutorial/security#15-do-not-use-openexternal-with-untrusted-content
+  */
+  contents.setWindowOpenHandler(({ url }) => {
+    const allowedOrigins : ReadonlySet<string> =
+      new Set<`https://${string}`>([ // Do not use insecure protocols like HTTP. https://www.electronjs.org/docs/latest/tutorial/security#1-only-load-secure-content
+      'https://vitejs.dev',
+      'https://github.com',
+      'https://v3.vuejs.org']);
+    const { origin } = new URL(url);
+    if (allowedOrigins.has(origin)){
+      shell.openExternal(url);
+    } else {
+      console.warn('Blocked the opening of an unallowed origin:', origin);
+    }
+    return { action: 'deny' };
+  });
+});
 
 
 app.on('second-instance', () => {
