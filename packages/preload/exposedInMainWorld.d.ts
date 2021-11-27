aa766106ae20d15c87de3ea6273458ea84778471
Alex Kozack <cawa-93@users.noreply.github.com>
2021-11-27 15:01:31 +0200

    chore: Update auto-generated

--- packages/preload/exposedInMainWorld.d.ts
+++ packages/preload/exposedInMainWorld.d.ts
@@ -1,3 +1,9 @@
 interface Window {
-    'electron': { readonly versions: NodeJS.ProcessVersions; };
+    /**
+     * The "Main World" is the JavaScript context that your main renderer code runs in.
+     * By default, the page you load in your renderer executes code in this world.
+     *
+     * @see https://www.electronjs.org/docs/api/context-bridge
+     */
+    readonly electron: { readonly versions: NodeJS.ProcessVersions; };
 }
