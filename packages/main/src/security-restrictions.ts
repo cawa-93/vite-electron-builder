140bd8ed6c18030f18951960c81a14864dc7edaa
Alex Kozack <cawa-93@users.noreply.github.com>
2022-01-01 15:17:19 +0200

    chore: Reduce `ALLOWED_EXTERNAL_ORIGINS`

--- packages/main/src/security-restrictions.ts
+++ packages/main/src/security-restrictions.ts
@@ -23,9 +23,7 @@ const ALLOWED_ORIGINS_AND_PERMISSIONS = new Map<string, Set<'clipboard-read' | '
  * >
  */
 const ALLOWED_EXTERNAL_ORIGINS = new Set<`https://${string}`>([
-  'https://vitejs.dev',
   'https://github.com',
-  'https://v3.vuejs.org',
 ]);
 
 
