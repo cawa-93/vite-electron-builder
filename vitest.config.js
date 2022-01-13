453731d79409c86448b79d9ac63b5cd5262bda54
Alex Kozack <cawa-93@users.noreply.github.com>
2022-01-13 16:48:19 +0200

    test: Increased timeout for e2e tests

--- vitest.config.js
+++ vitest.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite';
-
 /**
  * Config for global end-to-end tests
  * placed in project root tests folder
+ * @type {import('vite').UserConfig}
+ * @see https://vitest.dev/config/
  */
-export default defineConfig({
+const config = {
   test: {
     /**
      * By default, vitest search test files in all packages.
      * For e2e tests have sense search only is project root tests folder
      */
     include: ['./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+    /**
+     * A default timeout of 5000ms is sometimes not enough for playwright.
+     */
+    testTimeout: 30_000,
+    hookTimeout: 30_000,
   },
-});
+};
+
+export default config;
