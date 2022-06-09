1f2661bdd5c67cf34d035905b2f7bae357f84586
Alex Kozack <cawa-93@users.noreply.github.com>
2022-06-09 19:32:23 +0300

    refactor: Cache electron version in `postinstall` script
    resolve https://github.com/cawa-93/vite-electron-builder/issues/208

--- scripts/update-electron-vendors.js
+++ scripts/update-electron-vendors.js
@@ -1,45 +1,15 @@
-const {writeFile} = require('fs/promises');
-const {execSync} = require('child_process');
-const electron = require('electron');
+const {writeFileSync} = require('fs');
 const path = require('path');
 
-/**
- * Returns versions of electron vendors
- * The performance of this feature is very poor and can be improved
- * @see https://github.com/electron/electron/issues/28006
- *
- * @returns {NodeJS.ProcessVersions}
- */
-function getVendors() {
-  const output = execSync(`${electron} -p "JSON.stringify(process.versions)"`, {
-    env: {'ELECTRON_RUN_AS_NODE': '1'},
-    encoding: 'utf-8',
-  });
+const electronRelease = process.versions;
 
-  return JSON.parse(output);
-}
+const node = electronRelease.node.split('.')[0];
+const chrome = electronRelease.v8.split('.').splice(0, 2).join('');
 
-function updateVendors() {
-  const electronRelease = getVendors();
+const browserslistrcPath = path.resolve(process.cwd(), '.browserslistrc');
 
-  const nodeMajorVersion = electronRelease.node.split('.')[0];
-  const chromeMajorVersion = electronRelease.v8.split('.').splice(0, 2).join('');
+writeFileSync('./.electron-vendors.cache.json',
+  JSON.stringify({chrome, node}),
+);
 
-  const browserslistrcPath = path.resolve(process.cwd(), '.browserslistrc');
-
-  return Promise.all([
-    writeFile('./.electron-vendors.cache.json',
-      JSON.stringify({
-        chrome: chromeMajorVersion,
-        node: nodeMajorVersion,
-      }, null, 2) + '\n',
-    ),
-
-    writeFile(browserslistrcPath, `Chrome ${chromeMajorVersion}\n`, 'utf8'),
-  ]);
-}
-
-updateVendors().catch(err => {
-  console.error(err);
-  process.exit(1);
-});
+writeFileSync(browserslistrcPath, `Chrome ${chrome}`, 'utf8');
