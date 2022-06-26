a89b910dc69be7be3c012d0d3a7f96a1695d0385
Alex Kozack <cawa-93@users.noreply.github.com>
2022-06-26 13:59:35 +0300

    ci: Increase e2e `testTimeout`

--- vitest.config.js
+++ vitest.config.js
@@ -15,8 +15,8 @@ const config = {
     /**
      * A default timeout of 5000ms is sometimes not enough for playwright.
      */
-    testTimeout: 30_000,
-    hookTimeout: 30_000,
+    testTimeout: 300_000,
+    hookTimeout: 300_000,
   },
 };
 
