7334e35efca7e505288e4bedfb46743335c46c97
Alex Kozack <cawa-93@users.noreply.github.com>
2022-07-31 17:59:03 +0300

    refactor: Rewrite update-electron-vendors.js to ESM

--- scripts/update-electron-vendors.mjs
+++ scripts/update-electron-vendors.mjs
@@ -1,5 +1,11 @@
-const {writeFileSync} = require('fs');
-const path = require('path');
+/**
+ * This script should be run in electron context
+ * @example
+ *  ELECTRON_RUN_AS_NODE=1 electron scripts/update-electron-vendors.mjs
+ */
+
+import {writeFileSync} from 'fs';
+import path from 'path';
 
 const electronRelease = process.versions;
 
@@ -8,8 +14,5 @@ const chrome = electronRelease.v8.split('.').splice(0, 2).join('');
 
 const browserslistrcPath = path.resolve(process.cwd(), '.browserslistrc');
 
-writeFileSync('./.electron-vendors.cache.json',
-  JSON.stringify({chrome, node}),
-);
-
+writeFileSync('./.electron-vendors.cache.json', JSON.stringify({chrome, node}));
 writeFileSync(browserslistrcPath, `Chrome ${chrome}`, 'utf8');
