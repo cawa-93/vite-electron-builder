a2fc720e954475605dcb242d6cd9f071a179e0a8
Alex Kozack <cawa-93@users.noreply.github.com>
2022-08-08 15:14:51 +0300

    chore: run typechecking on committing dependencies changes

--- .nano-staged.mjs
+++ .nano-staged.mjs
@@ -4,18 +4,19 @@ export default {
   '*.{js,ts,vue}': 'eslint --cache --fix',
 
   /**
-   * Run typechecking if any type-sensitive files was staged
+   * Run typechecking if any type-sensitive files or project dependencies was changed
    * @param {string[]} filenames
    * @return {string[]}
    */
-  'packages/**/{*.ts,*.vue,tsconfig.json}': ({filenames}) => {
-    const pathToPackages = resolve(process.cwd(), 'packages') + sep;
-    return Array.from(
-      filenames.reduce((set, filename) => {
-        const pack = filename.replace(pathToPackages, '').split(sep)[0];
-        set.add(`npm run typecheck:${pack} --if-present`);
-        return set;
-      }, new Set),
-    );
+  '{package-lock.json,packages/**/{*.ts,*.vue,tsconfig.json}}': ({filenames}) => {
+    // if dependencies was changed run type checking for all packages
+    if (filenames.some(f => f.endsWith('package-lock.json'))) {
+      return ['npm run typecheck --if-present']
+    }
+
+    // else run type checking for staged packages
+    const fileNameToPackageName = filename => filename.replace(resolve(process.cwd(), 'packages') + sep, '').split(sep)[0]
+    return [...new Set(filenames.map(fileNameToPackageName))]
+      .map(p => `npm run typecheck:${p} --if-present`)
   },
 };
