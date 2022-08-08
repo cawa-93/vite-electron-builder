3ffc3b2a9fa4e9b9b5123ea8147e4c8122bad625
Alex Kozack <cawa-93@users.noreply.github.com>
2022-08-08 13:42:06 +0300

    fix: explicit disabled sandbox

--- packages/main/src/mainWindow.ts
+++ packages/main/src/mainWindow.ts
@@ -6,10 +6,9 @@ async function createWindow() {
   const browserWindow = new BrowserWindow({
     show: false, // Use the 'ready-to-show' event to show the instantiated BrowserWindow.
     webPreferences: {
-
       nodeIntegration: false,
       contextIsolation: true,
-
+      sandbox: false, // Sandbox disabled because the demo of preload script depend on the Node.js api
       webviewTag: false, // The webview tag is not recommended. Consider alternatives like an iframe or Electron's BrowserView. @see https://www.electronjs.org/docs/latest/api/webview-tag#warning
       preload: join(app.getAppPath(), 'packages/preload/dist/index.cjs'),
     },
