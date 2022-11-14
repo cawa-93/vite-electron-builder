70dd71396e5d6d72d6f8c00615984ebc6bb20499
Alex Kozack <cawa-93@users.noreply.github.com>
2022-11-15 01:34:19 +0200

    fix: Add `VITE_APP_VERSION` to env.d.ts

--- types/env.d.ts
+++ types/env.d.ts
@@ -6,7 +6,7 @@
  *
  * Note: To prevent accidentally leaking env variables to the client, only variables prefixed with `VITE_` are exposed to your Vite-processed code.
  *
- * @see https://github.com/vitejs/vite/blob/cab55b32de62e0de7d7789e8c2a1f04a8eae3a3f/packages/vite/types/importMeta.d.ts#L62-L69 Base Interface.
+ * @see https://github.com/vitejs/vite/blob/0a699856b248116632c1ac18515c0a5c7cf3d1db/packages/vite/types/importMeta.d.ts#L7-L14 Base Interface.
  * @see https://vitejs.dev/guide/env-and-mode.html#env-files Vite Env Variables Doc.
  */
 interface ImportMetaEnv {
@@ -15,10 +15,11 @@ interface ImportMetaEnv {
    * This variable is initialized in scripts/watch.ts
    */
   readonly VITE_DEV_SERVER_URL: undefined | string;
+
+  /** Current app version */
+  readonly VITE_APP_VERSION: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
-
-declare const __MYAPPVER__: string;
