51236a693761a56fea05655abc0f6f1632074501
Alex Kozack <cawa-93@users.noreply.github.com>
2022-11-17 14:16:26 +0200

    chore: Add .vscode/launch.json with debugger for main process
    resolve #859

--- scripts/watch.mjs
+++ scripts/watch.mjs
@@ -45,7 +45,7 @@ function setupMainPackageWatcher({resolvedUrls}) {
           }
 
           /** Spawn new electron process */
-          electronApp = spawn(String(electronPath), ['.'], {
+          electronApp = spawn(String(electronPath), ['--inspect','.'], {
             stdio: 'inherit',
           });
 
