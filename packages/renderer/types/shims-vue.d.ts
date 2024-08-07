93dd8c717122a9602de70a5e447b2408e8cfd05c
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-08-07 17:34:46 +0100

    fix(types): correct ESLint disable comment in shims-vue.d.ts
    Update the ESLint disable comment to use the correct rule:
    @typescript-eslint/no-empty-object-type instead of @typescript-eslint/ban-types
    which is deprecated and triggers an eslint error

--- packages/renderer/types/shims-vue.d.ts
+++ packages/renderer/types/shims-vue.d.ts
@@ -1,6 +1,6 @@
 declare module '*.vue' {
   import type {DefineComponent} from 'vue';
-  // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-object-type
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
