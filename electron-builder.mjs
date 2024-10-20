12452028d779bc7e6b32f4158ec49d7f19dc201b
Олександр Козак <kozackunisoft@gmail.com>
2024-10-20 16:34:19 +0300

    feat: reworked as npm workspaces

---
+++ electron-builder.mjs
@@ -0,0 +1,9 @@
+export default {
+  directories: {
+    output: 'dist',
+    buildResources: 'buildResources',
+  },
+  files: [
+    'packages/index.js',
+  ],
+};
--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -4,47 +4,50 @@ import {restoreOrCreateWindow} from '/@/mainWindow.js';
 import {platform} from 'node:process';
 import updater from 'electron-updater';
 
-/**
- * Prevent electron from running multiple instances.
- */
-const isSingleInstance = app.requestSingleInstanceLock();
-if (!isSingleInstance) {
-  app.quit();
-  process.exit(0);
-}
-app.on('second-instance', restoreOrCreateWindow);
-
-/**
- * Disable Hardware Acceleration to save more system resources.
- */
-app.disableHardwareAcceleration();
-
-/**
- * Shout down background process if all windows was closed
- */
-app.on('window-all-closed', () => {
-  if (platform !== 'darwin') {
+// noinspection JSUnusedGlobalSymbols
+// Used in packages/entry-point.js
+export function initApp() {
+  /**
+   * Prevent electron from running multiple instances.
+   */
+  const isSingleInstance = app.requestSingleInstanceLock();
+  if (!isSingleInstance) {
     app.quit();
+    process.exit(0);
   }
-});
+  app.on('second-instance', restoreOrCreateWindow);
 
-/**
- * @see https://www.electronjs.org/docs/latest/api/app#event-activate-macos Event: 'activate'.
- */
-app.on('activate', restoreOrCreateWindow);
+  /**
+   * Disable Hardware Acceleration to save more system resources.
+   */
+  app.disableHardwareAcceleration();
 
-/**
- * Create the application window when the background process is ready.
- */
-app
-  .whenReady()
-  .then(restoreOrCreateWindow)
-  .catch(e => console.error('Failed create window:', e));
+  /**
+   * Shout down background process if all windows was closed
+   */
+  app.on('window-all-closed', () => {
+    if (platform !== 'darwin') {
+      app.quit();
+    }
+  });
 
-/**
- * Install Vue.js or any other extension in development mode only.
- * Note: You must install `electron-devtools-installer` manually
- */
+  /**
+   * @see https://www.electronjs.org/docs/latest/api/app#event-activate-macos Event: 'activate'.
+   */
+  app.on('activate', restoreOrCreateWindow);
+
+  /**
+   * Create the application window when the background process is ready.
+   */
+  app
+    .whenReady()
+    .then(restoreOrCreateWindow)
+    .catch(e => console.error('Failed create window:', e));
+
+  /**
+   * Install Vue.js or any other extension in development mode only.
+   * Note: You must install `electron-devtools-installer` manually
+   */
 // if (import.meta.env.DEV) {
 //   app
 //     .whenReady()
@@ -63,18 +66,19 @@ app
 //     .catch(e => console.error('Failed install extension:', e));
 // }
 
-/**
- * Check for app updates, install it in background and notify user that new version was installed.
- * No reason run this in non-production build.
- * @see https://www.electron.build/auto-update.html#quick-setup-guide
- *
- * Note: It may throw "ENOENT: no such file app-update.yml"
- * if you compile production app without publishing it to distribution server.
- * Like `npm run compile` does. It's ok 😅
- */
-if (import.meta.env.PROD) {
-  app
-    .whenReady()
-    .then(() => updater.autoUpdater.checkForUpdatesAndNotify())
-    .catch(e => console.error('Failed check and install updates:', e));
+  /**
+   * Check for app updates, install it in background and notify user that new version was installed.
+   * No reason run this in non-production build.
+   * @see https://www.electron.build/auto-update.html#quick-setup-guide
+   *
+   * Note: It may throw "ENOENT: no such file app-update.yml"
+   * if you compile production app without publishing it to distribution server.
+   * Like `npm run compile` does. It's ok 😅
+   */
+  if (import.meta.env.PROD) {
+    app
+      .whenReady()
+      .then(() => updater.autoUpdater.checkForUpdatesAndNotify())
+      .catch(e => console.error('Failed check and install updates:', e));
+  }
 }
