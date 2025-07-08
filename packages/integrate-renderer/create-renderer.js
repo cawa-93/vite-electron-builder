354b2a5c4c9fda901befa1a2f12968bf579e66fd
Олександр Козак <kozackunisoft@gmail.com>
2025-07-08 20:02:06 +0300

    fix template scaffolding

--- packages/integrate-renderer/create-renderer.js
+++ packages/integrate-renderer/create-renderer.js
@@ -2,5 +2,7 @@ import { execSync } from 'child_process';
 import packageJson from '../../package.json' with {type: 'json'};
 
 const viteVersion = packageJson.devDependencies['create-vite'] ?? 'latest';
-
-execSync(`npm create vite@${viteVersion} renderer`, { stdio: 'inherit' });
+const templateIndex = process.argv.findIndex(arg => arg.startsWith('--template'))
+let template = templateIndex !== -1 ? process.argv[templateIndex + 1] : '';
+template = template ? `-- --template ${template}` : '';
+execSync(`npm create vite@${viteVersion} renderer ${template}`, { stdio: 'inherit' });
