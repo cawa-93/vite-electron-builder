69b0ac4d92029c7463bddfe8c77b01058ae69cfb
Олександр Козак <kozackunisoft@gmail.com>
2025-07-08 20:06:49 +0300

    fix extension types

--- packages/main/src/modules/ChromeDevToolsExtension.ts
+++ packages/main/src/modules/ChromeDevToolsExtension.ts
@@ -5,30 +5,22 @@ import installer from 'electron-devtools-installer';
 const {
   REDUX_DEVTOOLS,
   VUEJS_DEVTOOLS,
-  VUEJS3_DEVTOOLS,
   EMBER_INSPECTOR,
   BACKBONE_DEBUGGER,
   REACT_DEVELOPER_TOOLS,
-  APOLLO_DEVELOPER_TOOLS,
   JQUERY_DEBUGGER,
-  ANGULARJS_BATARANG,
   MOBX_DEVTOOLS,
-  CYCLEJS_DEVTOOL,
   default: installExtension,
 } = installer;
 
 const extensionsDictionary = {
   REDUX_DEVTOOLS,
   VUEJS_DEVTOOLS,
-  VUEJS3_DEVTOOLS,
   EMBER_INSPECTOR,
   BACKBONE_DEBUGGER,
   REACT_DEVELOPER_TOOLS,
-  APOLLO_DEVELOPER_TOOLS,
   JQUERY_DEBUGGER,
-  ANGULARJS_BATARANG,
   MOBX_DEVTOOLS,
-  CYCLEJS_DEVTOOL,
 } as const;
 
 export class ChromeDevToolsExtension implements AppModule {
