7b3a4db403fb52228bfe3a39a302b104ea86ef83
alias-ktc <alenpeni.info@gmail.com>
2025-12-16 11:51:06 +1000

    Added --no-immediate flag to create-renderer to ensure vite doesn't install package deps before renderer is renamed to @app/renderer

--- packages/integrate-renderer/create-renderer.js
+++ packages/integrate-renderer/create-renderer.js
@@ -4,9 +4,14 @@ import packageJson from '../../package.json' with {type: 'json'};
 const viteVersion = packageJson.devDependencies['create-vite'] ?? 'latest';
 const templateIndex = process.argv.findIndex(arg => arg.startsWith('--template'))
 let template = templateIndex !== -1 ? process.argv[templateIndex + 1] : '';
-template = template ? `-- --template ${template}` : '';
+
+// Vite Flags & Options
+template = template ? `--template ${template}` : '';
+const noImmediate = `--no-immediate`
+const viteFlags = `-- ${template} ${noImmediate}`;
+
 try {
-    execSync(`npm create vite@${viteVersion} renderer ${template}`, { stdio: 'inherit' });
+    execSync(`npm create vite@${viteVersion} renderer ${viteFlags}`, { stdio: 'inherit' });
 } catch (error) {
     console.error('Failed to execute the `npm create vite` command. Please check the Vite version, template, and your network connection.');
     console.error('Error details:', error.message);
